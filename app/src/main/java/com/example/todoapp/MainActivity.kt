05e66f7ed68b403cf341d9103f4d20df2e4069cc
Swistusmen <michal76aw@gmail.com>
2021-06-29 18:07:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+}
